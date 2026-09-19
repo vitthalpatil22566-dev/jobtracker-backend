253b483f4efdafaaf5a9451f00d0a2bc5ffab74a
Vaishnvi Patil <vitthalpatil22566@gmail.com>
2026-09-19 14:44:05 +0530

    Add Job entity with JPA annotations

--- src/main/java/com/jobtracker/entity/Job.java
+++ src/main/java/com/jobtracker/entity/Job.java
@@ -1,1 +1,29 @@
 
+package com.jobtracker.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "jobs")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Job {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String company;
+    private String position;
+    private String status;
+    private LocalDate appliedDate;
+    private String location;
+    private String jobType;
+    private String notes;
+}
