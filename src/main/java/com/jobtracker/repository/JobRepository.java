bd50e0f26e34b22afc0d01901713fcc5ad6f9cb7
Vaishnvi Patil <vitthalpatil22566@gmail.com>
2026-09-19 14:44:27 +0530

    Add JobRepository interface with custom query methods

--- src/main/java/com/jobtracker/repository/JobRepository.java
+++ src/main/java/com/jobtracker/repository/JobRepository.java
@@ -1,1 +1,17 @@
 
+package com.jobtracker.repository;
+
+import com.jobtracker.entity.Job;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface JobRepository extends JpaRepository<Job, Long> {
+
+    List<Job> findByStatus(String status);
+
+    List<Job> findByCompanyContainingIgnoreCaseOrPositionContainingIgnoreCase(
+            String company,
+            String position
+    );
+}
