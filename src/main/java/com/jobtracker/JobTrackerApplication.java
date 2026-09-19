90d5809ed737a9fe4ffcff84958aa520fef564fb
Vaishnvi Patil <vitthalpatil22566@gmail.com>
2026-09-19 14:45:06 +0530

    Initialize JobTrackerApplication with Spring Boot

--- src/main/java/com/jobtracker/JobTrackerApplication.java
+++ src/main/java/com/jobtracker/JobTrackerApplication.java
@@ -1,1 +1,13 @@
 
+package com.jobtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobTrackerApplication.class, args);
+    }
+}
